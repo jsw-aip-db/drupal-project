65ae55b54e65a55873cb58531894da464c94bd22
Philipp <157365619+phma4jsw@users.noreply.github.com>
2024-05-07 11:39:31 +0000

    Update drush, Readme and split config into did and dod

--- .devcontainer/init-container.sh
+++ .devcontainer/init-container.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 ddev config global --instrumentation-opt-in=false
-mkcert -install
+[ $CODESPACES ] || mkcert -install
 composer install --ignore-platform-reqs
+
+[ $CODESPACES ] || echo "Add \`rootCA.pem\` to your host's trust store or replace it with your own!"
+echo "Use \`ddev start\` to run the Drupal site."
